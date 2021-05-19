bced7c3c99be0b93df3ff9bc2f4d823878a05a00
Christian Trenkwalder <christian@karatemuffin.it>
2021-05-19 20:20:49 +0200

    parser fruehwarnung

---
+++ parser.py
@@ -0,0 +1,27 @@
+import re
+import csv
+from py_pdf_parser.loaders import load_file
+
+document = load_file("combined.pdf")
+
+with open('data.csv', 'w', newline='') as csvfile:
+
+	writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+	writer.writerow(['Form1','Form2','Form3','Form4','Form5','Form6'])
+
+	for pagenumber in range(1,document.number_of_pages,2):
+		page = document.get_page(pagenumber)
+		adresse = re.findall('.*\n?',page.elements[0].text())
+		
+		page = document.get_page(pagenumber + 1 )
+		name = re.findall('Name: (.*)\((.*)\)',page.elements[1].text() )
+
+		res = []
+		for item in adresse:
+			res.append(item.replace("\n","")) if len(item) > 0 else 0
+			
+			
+		for item in name[0]:
+			res.append(item)
+		print(res)
+		writer.writerow(res)
